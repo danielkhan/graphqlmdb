1f6440c130edc403c280b209313687aa12b67081
Juan Garcia <juan_garcia_1995@hotmail.com>
2019-07-17 12:10:07 -0500

    wrapped content on the home page inside a container

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -4,11 +4,15 @@ import { Link } from "react-router-dom";
 const Home = () => {
   return (
     <div className="hero">
-      <div>
-        <p>Discover new movies and see what others recommend</p>
-        <Link to="/movies" className="btn btn--mobile btn--desktop">
-          Discover
-        </Link>
+      <div className="container">
+        <div className="hero-wrapper">
+          <div>
+            <p>Discover new movies and see what others recommend</p>
+            <Link to="/movies" className="btn btn--mobile btn--desktop">
+              Discover
+            </Link>
+          </div>
+        </div>
       </div>
     </div>
   );
