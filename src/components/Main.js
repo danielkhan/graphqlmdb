6f15c21fe33bb71e2b6fe4465b8e5877adfe78e0
Juan Garcia <juan_garcia_1995@hotmail.com>
2019-07-18 16:28:16 -0500

    show loading status when making fetch requests

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,97 +1,120 @@
-import React, { useState, useEffect, useContext } from "react";
-import { Link } from "react-router-dom";
-
-import { GenreContext } from "../contexts/GenreContext";
-import { ConfigContext } from "../contexts/ConfigContext";
+import React, { useState, useEffect } from "react";
+import Movie from "./Movie";
+import Loader from "./Loader";
 
 import Axios from "axios";
 
 const Main = () => {
-  const [results, setResults] = useState([]);
-  const [loading, setLoading] = useState(true);
+  const [moviesInTheaters, setMoviesInTheaters] = useState([]);
+  const [loadingMoviesInTheaters, setLoadingMoviesInTheaters] = useState(true);
 
-  const { images } = useContext(ConfigContext);
-  const { genres } = useContext(GenreContext);
+  const [upcomingMovies, setUpcomingMovies] = useState([]);
+  const [loadingUpcomingMovies, setLoadingUpcomingMovies] = useState(true);
+
+  const [popularMovies, setPopularMovies] = useState([]);
+  const [loadingPopularMovies, setLoadingPopularMovies] = useState(true);
+
+  const [activeInTheatersLink, setActiveInTheatersLink] = useState(false);
+  const [activeComingSoonLink, setActiveComingSoonLink] = useState(false);
+  const [activePopularLink, setActivePopularLink] = useState(false);
 
   useEffect(() => {
     fetchMovieInTheaters();
-    fetchUpcomingMovies();
-    fetchPopularMovies();
   }, []);
 
   const fetchMovieInTheaters = () => {
+    setActivePopularLink(false);
+    setActiveComingSoonLink(false);
+    setActiveInTheatersLink(true);
+
+    setMoviesInTheaters([]);
     Axios.get("/.netlify/functions/now_playing").then(res => {
-      setResults(res.data.results);
-      setLoading(false);
+      setMoviesInTheaters(res.data.results);
+      setLoadingMoviesInTheaters(false);
     });
   };
 
   const fetchUpcomingMovies = () => {
+    setActiveInTheatersLink(false);
+    setActiveComingSoonLink(true);
+    setActivePopularLink(false);
+
+    setUpcomingMovies([]);
     Axios.get("/.netlify/functions/upcoming_movies").then(res => {
-      setResults(res.data.results);
-      setLoading(false);
+      setUpcomingMovies(res.data.results);
+      setLoadingUpcomingMovies(false);
     });
   };
 
   const fetchPopularMovies = () => {
+    setActiveComingSoonLink(false);
+    setActiveInTheatersLink(false);
+    setActivePopularLink(true);
+
+    setPopularMovies([]);
     Axios.get("/.netlify/functions/popular_movies").then(res => {
-      setResults(res.data.results);
-      setLoading(false);
+      setPopularMovies(res.data.results);
+      setLoadingPopularMovies(false);
     });
   };
 
-  const path = images.secure_base_url;
-  const imgSize = images.poster_sizes[3];
-
-  if (loading) {
+  if (loadingMoviesInTheaters) {
     return false;
   }
 
   return (
     <main className="home-main">
       <ul className="nav">
-        <li className="nav__item" onClick={fetchMovieInTheaters}>
+        <li
+          className={`nav__item ${activeInTheatersLink ? "active-link" : ""}`}
+          onClick={fetchMovieInTheaters}
+        >
           In Theaters
         </li>
-        <li className="nav__item" onClick={fetchUpcomingMovies}>
+        <li
+          className={`nav__item ${activeComingSoonLink ? "active-link" : ""}`}
+          onClick={fetchUpcomingMovies}
+        >
           Coming Soon
         </li>
-        <li className="nav__item" onClick={fetchPopularMovies}>
+        <li
+          className={`nav__item ${activePopularLink ? "active-link" : ""}`}
+          onClick={fetchPopularMovies}
+        >
           Popular
         </li>
       </ul>
       <section className="flex">
-        {results.map(r => (
-          <Link to={`/details/${r.id}`} key={r.id}>
-            <figure className="movie">
-              <img
-                className="movie__poster"
-                src={`${path}${imgSize}${r.poster_path}`}
-                alt={`${r.title || r.name}`}
-              />
-              <figcaption>
-                <h3 className="movie__title">{r.title || r.name}</h3>
-                <ul className="movie__genre">
-                  {r.genre_ids
-                    .map(gid =>
-                      genres.filter(g => g.id === gid).map(g => g.name)
-                    )
-                    .reduce((prev, next) => prev.concat(next))
-                    .slice(0, 3)
-                    .map((genre, i) => (
-                      <li key={i} className="movie__genre-item">
-                        {genre}
-                      </li>
-                    ))}
-                </ul>
-                <div className="movie__rating">
-                  <i className="movie__icon fas fa-heart" />
-                  <span className="movie__number">{r.vote_average}</span>
-                </div>
-              </figcaption>
-            </figure>
-          </Link>
-        ))}
+        {activeInTheatersLink
+          ? loadingMoviesInTheaters
+            ? <Loader />
+            : moviesInTheaters.map(m => (
+                <Movie
+                  key={m.id}
+                  movie={m}
+                />
+              ))
+          : null}
+        {activeComingSoonLink
+          ? loadingUpcomingMovies
+            ? <Loader />
+            : upcomingMovies.map(m => (
+                <Movie
+                  key={m.id}
+                  movie={m}
+                />
+              ))
+          : null}
+        {activePopularLink
+          ? loadingPopularMovies
+            ? <Loader />
+            : popularMovies.map(m => (
+                <Movie
+                  key={m.id}
+                  movie={m}
+                />
+              ))
+          : null}
       </section>
     </main>
   );
